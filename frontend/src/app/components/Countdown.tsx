19d52707622fde8da0f6e23550fe978c555a6204
Euan Caskie <56805259+Ortovoxx@users.noreply.github.com>
2024-03-17 06:49:13 +0000

    Update Countdown.tsx

--- frontend/src/app/components/Countdown.tsx
+++ frontend/src/app/components/Countdown.tsx
@@ -2,9 +2,7 @@
 
 import {useEffect, useState} from "react";
 
-export const Countdown = ({ textSize }: { textSize: string }) => {
-
-    const timeLeft = 300;
+export const Countdown = ({ textSize, timeLeft }: { textSize: string, timeLeft: number }) => {
 
     const [counter, setCounter] = useState(timeLeft);
 
