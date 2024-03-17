f115ce0a053862272ee391f1723799031540e85e
Euan Caskie <56805259+Ortovoxx@users.noreply.github.com>
2024-03-17 09:22:45 +0000

    Update page.tsx

--- frontend/src/app/explore/page.tsx
+++ frontend/src/app/explore/page.tsx
@@ -14,196 +14,6 @@ import {IProfile} from "@/app/components/Profile";
 export default function StartPage() {
 
 
-    const posts = [
-        {
-            id: "tsdf",
-            username: "miam-miam100",
-            profile_picture: "https://avatars.githubusercontent.com/u/49870539?v=4",
-            title: "My first commit",
-            description: "I just made",
-            language: "python",
-            code: "def twoSum(self, nums: List[int], target: int) -> List[int]:\n    pass",
-            reactions: {
-                heart: 2,
-                rocket: 0,
-                thumbsup: 0,
-                thumbsdown: 0,
-                skull: 0,
-                trash: 0,
-                tada: 0,
-                facepalm: 0,
-                nerd: 29,
-            },
-            user_reactions: {
-                heart: true,
-                rocket: false,
-                thumbsup: false,
-                thumbsdown: false,
-                skull: false,
-                trash: false,
-                tada: false,
-                facepalm: false,
-                nerd: false,
-            }
-        },
-        {
-            id: "tsdf",
-            username: "ortovoxx",
-            profile_picture: "https://avatars.githubusercontent.com/u/56805259?v=4",
-            title: "My first commit",
-            description: "Just a test to see a longer message and how it looks like",
-            language: "typescript",
-            code: "var twoSum = function(nums, target) {\n//test\n//test\n//test\n//test\n//test\n//test\n//test\n//test\n//test\n  \n};",
-            reactions: {
-                heart: 2,
-                rocket: 0,
-                thumbsup: 0,
-                thumbsdown: 0,
-                skull: 0,
-                trash: 0,
-                tada: 0,
-                facepalm: 0,
-                nerd: 0,
-            },
-            user_reactions: {
-                heart: false,
-                rocket: false,
-                thumbsup: false,
-                thumbsdown: false,
-                skull: false,
-                trash: false,
-                tada: false,
-                facepalm: false,
-                nerd: false,
-            }
-        },
-        {
-            id: "tsdf",
-            username: "ortovoxx",
-            profile_picture: "https://avatars.githubusercontent.com/u/56805259?v=4",
-            title: "My first commit",
-            description: "Just a test to see a longer message and how it looks like",
-            language: "typescript",
-            code: "var twoSum = function(nums, target) {\n//test\n//test\n//test\n//test\n//test\n//test\n//test\n//test\n//test\n  \n};",
-            reactions: {
-                heart: 2,
-                rocket: 0,
-                thumbsup: 0,
-                thumbsdown: 0,
-                skull: 0,
-                trash: 0,
-                tada: 0,
-                facepalm: 0,
-                nerd: 0,
-            },
-            user_reactions: {
-                heart: false,
-                rocket: false,
-                thumbsup: false,
-                thumbsdown: false,
-                skull: false,
-                trash: false,
-                tada: false,
-                facepalm: false,
-                nerd: false,
-            }
-        },
-        {
-            id: "tsdf",
-            username: "ortovoxx",
-            profile_picture: "https://avatars.githubusercontent.com/u/56805259?v=4",
-            title: "My first commit",
-            description: "Just a test to see a longer message and how it looks like",
-            language: "typescript",
-            code: "var twoSum = function(nums, target) {\n//test\n//test\n//test\n//test\n//test\n//test\n//test\n//test\n//test\n  \n};",
-            reactions: {
-                heart: 2,
-                rocket: 0,
-                thumbsup: 0,
-                thumbsdown: 0,
-                skull: 0,
-                trash: 0,
-                tada: 0,
-                facepalm: 0,
-                nerd: 0,
-            },
-            user_reactions: {
-                heart: false,
-                rocket: false,
-                thumbsup: false,
-                thumbsdown: false,
-                skull: false,
-                trash: false,
-                tada: false,
-                facepalm: false,
-                nerd: false,
-            }
-        },
-        {
-            id: "tsdf",
-            username: "ortovoxx",
-            profile_picture: "https://avatars.githubusercontent.com/u/56805259?v=4",
-            title: "My first commit",
-            description: "Just a test to see a longer message and how it looks like",
-            language: "typescript",
-            code: "var twoSum = function(nums, target) {\n//test\n//test\n//test\n//test\n//test\n//test\n//test\n//test\n//test\n  \n};",
-            reactions: {
-                heart: 2,
-                rocket: 0,
-                thumbsup: 0,
-                thumbsdown: 0,
-                skull: 0,
-                trash: 0,
-                tada: 0,
-                facepalm: 0,
-                nerd: 0,
-            },
-            user_reactions: {
-                heart: false,
-                rocket: false,
-                thumbsup: false,
-                thumbsdown: false,
-                skull: false,
-                trash: false,
-                tada: false,
-                facepalm: false,
-                nerd: false,
-            }
-        },
-        {
-            id: "tsdf",
-            username: "viktaur",
-            profile_picture: "https://avatars.githubusercontent.com/u/30535579?v=4",
-            title: "My first commit",
-            description: "I just made",
-            language: "python",
-            code: "def twoSum(self, nums: List[int], target: int) -> List[int]:\n    pass",
-            reactions: {
-                heart: 2,
-                rocket: 0,
-                thumbsup: 0,
-                thumbsdown: 0,
-                skull: 0,
-                trash: 0,
-                tada: 0,
-                facepalm: 0,
-                nerd: 0,
-            },
-            user_reactions: {
-                heart: false,
-                rocket: false,
-                thumbsup: false,
-                thumbsdown: false,
-                skull: false,
-                trash: false,
-                tada: false,
-                facepalm: false,
-                nerd: false,
-            }
-
-        }
-    ]
-
 
 
 
@@ -230,10 +40,34 @@ export default function StartPage() {
 
 
     if (postData === undefined || userData === undefined) {
-        return null;
+        return <div>Loading...</div>;
     }
 
-    const lockedPosts = postData.map((post) => ({...post, locked: !userData.done_recent}));
+    const lockedPosts = postData.map((post) => ({...post, locked: !userData.completed_correctly,
+        reactions: {
+                heart: 2,
+                rocket: 0,
+                thumbsup: 0,
+                thumbsdown: 0,
+                skull: 0,
+                trash: 0,
+                tada: 0,
+                facepalm: 0,
+                nerd: 29,
+            },
+            user_reactions: {
+                heart: true,
+                rocket: false,
+                thumbsup: false,
+                thumbsdown: false,
+                skull: false,
+                trash: false,
+                tada: false,
+                facepalm: false,
+                nerd: false,
+            }
+
+    }));
 
     const allPosts = lockedPosts.map((post, index) => {
         return (
