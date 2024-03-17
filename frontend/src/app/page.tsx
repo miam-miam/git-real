589ab16db4af6d7815c67fcd30c7fc6ec04762b3
Euan Caskie <56805259+Ortovoxx@users.noreply.github.com>
2024-03-17 11:09:44 +0000

    Dark mode fixes

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -6,23 +6,23 @@ export default function Home() {
         <div className="max-w-7xl relative flex place-items-center">
             <div className="w-5xl relative">
                 <div>
-                    <h1 className="text-7xl font-bold text-left mb-10">
+                    <h1 className="text-7xl font-bold text-left mb-10 text-white">
                         Your daily dose of code.
                     </h1>
                 </div>
                 <div className="mb-10">
-                    <h2 className="text-3xl font-bold text-left mb-2">
+                    <h2 className="text-3xl font-bold text-left mb-2 text-white">
                         😊 Meaningful commits
                     </h2>
-                    <h2 className="text-3xl font-bold text-left mb-2">
+                    <h2 className="text-3xl font-bold text-left mb-2 text-white">
                         ⚠️ Spontaneous merges
                     </h2>
-                    <h2 className="text-3xl font-bold text-left mb-2">
+                    <h2 className="text-3xl font-bold text-left mb-2 text-white">
                         🤳 Authentic pull requests
                     </h2>
                 </div>
                 <div>
-                    <h3 className="text-xl text-left mb-10">
+                    <h3 className="text-xl text-left mb-10 text-white">
                         Everyday get a notification to share a genuine glimpse into your coding life with the people
                         you care most about.
                     </h3>
