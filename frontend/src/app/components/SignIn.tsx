c0cb98225b6d04b1a5117c9be1676f8d74c433bf
miam-miam100 <49870539+miam-miam100@users.noreply.github.com>
2024-03-17 02:00:06 +0000

    Modify sign in button url

--- frontend/src/app/components/SignIn.tsx
+++ frontend/src/app/components/SignIn.tsx
@@ -6,7 +6,7 @@ import Link from "next/link";
 export const SignIn = () => {
 
     return (
-        <Link href={'http://localhost:3001/api/auth/signin'}>
+        <Link href={'http://localhost:3001/auth/login'}>
             <button type="button"
                     className="py-3 px-4 hover:invert inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-gray-50 text-black hover:bg-gray-200">
                 <Image
