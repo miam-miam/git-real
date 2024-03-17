f942d0a05adac63a00833671f503d14a0a7fb14d
Euan Caskie <56805259+Ortovoxx@users.noreply.github.com>
2024-03-17 00:08:36 +0000

    Create EditorWindow.tsx

---
+++ frontend/src/app/components/EditorWindow.tsx
@@ -0,0 +1,62 @@
+"use client";
+
+import React, {useEffect, useState} from "react";
+
+import Editor, {useMonaco} from "@monaco-editor/react";
+import {monacoTheme} from "@/app/components/MonacoTheme";
+
+export const CodeEditorWindow = ({onChange, language, boilerPlate}: {
+    onChange: (a: string, b: string) => any,
+    language: string,
+    boilerPlate: string
+}) => {
+    const [value, setValue] = useState(boilerPlate || "");
+
+    const monaco = useMonaco();
+
+    useEffect(() => {
+
+        monaco?.editor.defineTheme('custom', {
+            ...monacoTheme,
+            base: 'vs-dark',
+            inherit: true,
+        });
+
+        monaco?.editor.setTheme('custom')
+    }, [monaco]);
+
+    // https://microsoft.github.io/monaco-editor/typedoc/interfaces/editor.IStandaloneEditorConstructionOptions.html
+    // https://www.npmjs.com/package/@monaco-editor/react#editor
+    const options = {
+        selectOnLineNumbers: true,
+        scrollBeyondLastLine: false,
+        fontSize: 19,
+        overviewRulerLanes: 0,
+        minimap: {
+            enabled: false,
+        },
+        padding: {
+            top: 20,
+            bottom: 20
+        }
+    };
+
+    const handleEditorChange = (value: string | undefined) => {
+        setValue(value || "");
+        onChange("code", value || "");
+    };
+
+    return (
+        <div className="overlay rounded-md overflow-hidden w-full h-full shadow-4xl">
+            <Editor
+                height="20vh"
+                width={`100%`}
+                language={language || "javascript"}
+                value={boilerPlate}
+                defaultValue={boilerPlate}
+                onChange={handleEditorChange}
+                options={options}
+            />
+        </div>
+    );
+};
