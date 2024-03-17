f8b92b3528491ade5555c0840a4b351debb86107
Euan Caskie <56805259+Ortovoxx@users.noreply.github.com>
2024-03-17 06:49:39 +0000

    Create fetchChallenge.ts

---
+++ frontend/src/app/utilities/fetchChallenge.ts
@@ -0,0 +1,37 @@
+interface IChallenge {
+    id: number,
+    title: string,
+    description: string,
+    example_input: string,
+    example_output: string,
+    boilerplate: {
+        python: string,
+        typescript: string,
+        rust: string,
+    },
+    default_language: string,
+    date_released: string,
+    deadline: string,
+}
+
+
+export const fetchChallenge = async (): Promise<IChallenge | null> => {
+    const res = await fetch('http://localhost:3001/api/challenge', {
+        method: 'GET',
+        credentials: "include"
+    })
+
+    if (!res.ok) {
+        console.error("res challenge", res.status, res.statusText)
+
+    }
+
+    let data: IChallenge;
+
+    if (res.ok) {
+        data = await res.json()
+        return data;
+    }
+
+    return null;
+}
